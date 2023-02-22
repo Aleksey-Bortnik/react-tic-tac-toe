994301a17351108a63a8b7538f16b6320cda4f33
aleksey <aleksey.bortnik91@gmail.com>
2023-02-22 17:36:40 +0300

    changed type of Square component

--- my-app/src/Square.js
+++ my-app/src/Square.js
@@ -1,24 +1,32 @@
 import React from 'react';
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
+// class Square extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             value: null,
+//         };
+//     }
 
-    render() {
-        return (
-            <button className='square'
-                onClick={() => {
-                    this.setState({ value: 'X' })
-                }}>
-                {this.state.value}
-                {console.log(this)} 
-            </button>
-        );
-    }
+//     render() {
+//         return (
+//             <button className='square'
+//                 onClick={() => {
+//                     this.props.onClick()
+//                 }}>
+//                 {this.props.value}
+//                 {console.log(this)} 
+//             </button>
+//         );
+//     }
+// }
+
+function Square(props) {
+    return (
+        <button className='square' onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
 
 //we use arrows to bind the function to the class?
